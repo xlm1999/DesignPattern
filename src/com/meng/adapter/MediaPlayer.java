6ce718534b01e4efc2e38b8ee43a655fa80b3aa4
xlm1999 <xlm_1999@163.com>
2020-09-03 19:56:28 +0800

    my commit

---
+++ src/com/meng/adapter/MediaPlayer.java
@@ -0,0 +1,5 @@
+package com.meng.adapter;
+
+public interface MediaPlayer {
+    public void play(String audioType, String fileName);
+}
---
+++ src/com/meng/proxy/Image.java
@@ -0,0 +1,5 @@
+package com.meng.proxy;
+
+public interface Image {
+    void display();
+}
